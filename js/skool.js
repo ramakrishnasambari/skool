e854e3b1f4745ea562f7618c1330c0d2d9f07a41
ramakrishnasambari <ramakrishna469@gmail.com>
2022-08-07 11:39:07 +0530

    Add files via upload

---
+++ js/skool.js
@@ -0,0 +1,36 @@
+var skool = angular.module("skool", []);
+
+skool.config(['$httpProvider', function($httpProvider) {
+	$httpProvider.defaults.withCredentials = true;
+}])
+
+skool.config(['$qProvider', function($qProvider) {
+	$qProvider.errorOnUnhandledRejections(false);
+}]);
+
+skool.controller("basectrl", ["$scope", "$http", "$location", "$rootScope", function($scope, $http, $location, $rootScope) {
+
+	$scope.welcome = "Welcome to school";
+	
+	//GET Server Call
+	var getServerCall = function(url, callback) {
+		$http.get(url, { withCredentials: false, headers: $rootScope.header }).then(function(response) {
+			callback && callback(response.data);
+		});
+	}
+
+	//POST Server Call
+	var postServerCall = function(inputdata, url, callback) {
+		$http.post(url, inputdata).then(function(response) {
+			callback && callback(response.data);
+		});
+	}
+	
+	$scope.selectedMenu = "menu-home";
+	$scope.selectMenu = function(menu){
+		$(".menu").removeClass("active-menu");
+		$("."+menu).addClass("active-menu");
+		$scope.selectedMenu = menu;
+	}
+
+}]);
